835e10a7dc19572947c262fd6d4a5782115e627b
RisingGeek <bhavaypuri15@gmail.com>
2020-09-18 22:40:24 +0530

    Update database

--- src/sequelize.js
+++ src/sequelize.js
@@ -8,12 +8,19 @@ const CommentModel = require("./models/Comment");
 const BookmarkModel = require("./models/Bookmark");
 
 // Connect to database
-const { DB, USER, PASSWORD, HOST } = process.env;
+const { DB, USER, PASSWORD, HOST, DB_PORT } = process.env;
 const sequelize = new Sequelize(DB, USER, PASSWORD, {
   host: HOST,
+  port: DB_PORT,
   dialect: "mysql",
 });
-(async () => await sequelize.sync())();
+(async () => {
+  try {
+    const res = await sequelize.sync();
+  } catch (err) {
+    console.log(err);
+  }
+})();
 
 const User = UserModel(sequelize);
 const Follower = FollowerModel(sequelize);
@@ -41,7 +48,7 @@ Tweet.hasMany(Retweet, { foreignKey: "tweetId" });
 User.hasMany(Comment, { foreignKey: "userId" });
 // Tweet -> Bookmark association
 Tweet.hasMany(Bookmark, { foreignKey: "tweetId" });
-User.hasMany(Bookmark, {foreignKey: 'userId'})
+User.hasMany(Bookmark, { foreignKey: "userId" });
 
 module.exports = {
   User,
