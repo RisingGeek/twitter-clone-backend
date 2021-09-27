2d48bc421c9a5326512992a38a4c3658ca62fa6b
RisingGeek <bhavaypuri15@gmail.com>
2021-09-27 21:19:55 +0530

    allow origin

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 app.use(function (req, res, next) {
 
-    res.setHeader('Access-Control-Allow-Origin', 'https://twitter-geek.netlify.app/');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
