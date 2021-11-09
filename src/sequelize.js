496cf25a4344bb71b28d41e072229dbe2fec2200
RisingGeek <bhavay.puri@partsavatar.ca>
2021-11-09 22:55:42 +0530

    remove log

--- src/sequelize.js
+++ src/sequelize.js
@@ -9,7 +9,6 @@ const BookmarkModel = require("./models/Bookmark");
 
 // Connect to database
 const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
-console.log(process.env.USER)
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   port: DB_PORT,
